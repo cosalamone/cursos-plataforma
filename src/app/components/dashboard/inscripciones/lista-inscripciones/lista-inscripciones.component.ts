0810225830d5018db92cfa539fa59fc2eb92e256
cosalamone <cosalamone@gmail.com>
2023-05-18 19:45:55 -0300

    tabla de inscripciones

--- src/app/components/dashboard/inscripciones/lista-inscripciones/lista-inscripciones.component.ts
+++ src/app/components/dashboard/inscripciones/lista-inscripciones/lista-inscripciones.component.ts
@@ -4,6 +4,11 @@ import { InscripcionesActions } from '../store/inscripciones.actions';
 import { Observable } from 'rxjs';
 import { State } from '../store/inscripciones.reducer';
 import { selectInscripcionesState } from '../store/inscripciones.selectors';
+import { Alumno, Curso, Inscripcion } from 'src/interfaces';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { InscripcionesService } from 'src/app/services/inscripciones.service';
+import { CursosService } from 'src/app/services/cursos.service';
 
 @Component({
   selector: 'app-lista-inscripciones',
@@ -11,18 +16,59 @@ import { selectInscripcionesState } from '../store/inscripciones.selectors';
   styleUrls: ['./lista-inscripciones.component.scss']
 })
 export class ListaInscripcionesComponent implements OnInit {
+  panelOpenState = false;
+  inscripciones: Inscripcion[] | undefined;
+  alumno: Alumno | undefined;
+  cursosInscriptos: Curso[] | undefined;
+
+  displayedColumns: string[] = [
+    'idCurso',
+    'name',
+    'cantAlumnos',
+    'opciones'
+  ];
+
+  dataSource!: MatTableDataSource<any, any>
+
 
   state$: Observable<State>
 
-  constructor(private store: Store) {
+  constructor(private store: Store,
+    private activatedRoute: ActivatedRoute,
+    private inscripcionesService: InscripcionesService,
+    private cursosService: CursosService,) {
+
     this.state$ = this.store.select(selectInscripcionesState)
+
+    this.inscripcionesService.getInscripciones()
+    .subscribe((objeInscripciones) => {
+      this.inscripciones = objeInscripciones;
+      console.log(this.inscripciones)
+      this.cursosService
+        .getCursos()
+        .subscribe(
+          (dataCursos) => {
+            console.log(dataCursos)
+            this.cursosInscriptos = dataCursos.filter(x => this.inscripciones?.some(insc => insc.idCurso === x.id))
+            console.log(this.cursosInscriptos)
+
+            this.dataSource = new MatTableDataSource(this.cursosInscriptos as any)
+
+          }
+        );
+    })
   }
+
+
   ngOnInit(): void {
     this.store.dispatch(InscripcionesActions.loadInscripciones());
   }
 
-  eliminarInscripcionPorId(id: number): void{
-    this.store.dispatch(InscripcionesActions.deleteInscripcion({id}))
+
+  detalleAlumnosInscriptos(){}
+
+  eliminarInscripcionPorId(id: number): void {
+    this.store.dispatch(InscripcionesActions.deleteInscripcion({ id }))
   } // se usa en el html de la futura tabla de inscripciones - 
 
 }
