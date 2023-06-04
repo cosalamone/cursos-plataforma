aa27acba08c8ce06d83cecb82cb56fc637e344b3
cosalamone <cosalamone@gmail.com>
2023-06-04 12:00:35 -0300

    tablacursos muestra nombredocentes

--- src/app/components/dashboard/cursos/tabla-cursos/tabla-cursos.component.ts
+++ src/app/components/dashboard/cursos/tabla-cursos/tabla-cursos.component.ts
@@ -3,10 +3,11 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CursosService } from 'src/app/services/cursos.service';
-import { Alumno, Curso, Usuario } from 'src/interfaces';
+import { Alumno, Curso, Docente, Usuario } from 'src/interfaces';
 import { FormAbmCursosComponent } from './form-abm-cursos/form-abm-cursos.component';
 import { AuthService } from 'src/app/services/auth.service';
-import { Observable } from 'rxjs';
+import { Observable, firstValueFrom } from 'rxjs';
+import { DocentesService } from 'src/app/services/docentes.service';
 
 
 @Component({
@@ -18,7 +19,6 @@ export class TablaCursosComponent {
 
   authUserObs$: Observable<Usuario | null>;
 
-
   displayedColumns: string[] = [
     'id',
     'nombre',
@@ -28,6 +28,7 @@ export class TablaCursosComponent {
   ];
 
   dataSource!: MatTableDataSource<any, any>;
+  docentes!: Docente[];
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -41,6 +42,7 @@ export class TablaCursosComponent {
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private authService: AuthService,
+    private docentesService: DocentesService,
 
 
   ) {
@@ -52,23 +54,39 @@ export class TablaCursosComponent {
     this.cursosService
       .getCursos()
       .subscribe(
-        (data) => (this.dataSource = new MatTableDataSource(data as any))
+        async (data) => {
+          (this.dataSource = new MatTableDataSource(data as any))
+          this.docentes = await firstValueFrom(this.docentesService.getDocentes())
+        }
       );
   }
+
+  obtenerNombreApellidoDocentePorId(id: number): String | undefined {
+
+    let docente= this.docentes.find(x => x.id === id);
+    return `${docente?.nombre} ${docente?.apellido}`;
+  }
+
 
   abrirABMCurso(): void {
     const dialog = this.matDialog.open(FormAbmCursosComponent);
 
     dialog.afterClosed().subscribe((valor) => {
       if (valor) {
+
+
+        valor.docente.id
         let curso: Curso = valor;
         let newId = Math.max(...this.dataSource.data.map(x => x.id)) + 1;
 
         curso.id = newId;
 
+
+
+
         this.cursosService.postNewCurso(curso)
-        .subscribe()
-        this.dataSource.data= [...this.dataSource.data, curso];
+          .subscribe()
+        this.dataSource.data = [...this.dataSource.data, curso];
       }
     })
   }
@@ -80,18 +98,18 @@ export class TablaCursosComponent {
       }
     });
 
-    dialog.afterClosed().subscribe((valor)=> {
-      if (valor ){
+    dialog.afterClosed().subscribe((valor) => {
+      if (valor) {
         let curso: Curso = valor;
         let idCursoModificar = curso.id;
         let posicionAEditar = this.dataSource.data.findIndex(
-          (alumno)=> alumno.id === idCursoModificar
+          (alumno) => alumno.id === idCursoModificar
         );
-        this.dataSource.data[posicionAEditar]= curso;
+        this.dataSource.data[posicionAEditar] = curso;
 
         this.cursosService.putCurso(curso, idCursoModificar)
-        .subscribe()
-        this.dataSource= new MatTableDataSource(this.dataSource.data)
+          .subscribe()
+        this.dataSource = new MatTableDataSource(this.dataSource.data)
       }
     });
   }
@@ -103,10 +121,10 @@ export class TablaCursosComponent {
     this.dataSource.data.splice(posicionAEliminar, 1);
 
     this.cursosService.deleteCurso(idCursoAEliminar)
-    .subscribe()
+      .subscribe()
 
     this.dataSource.data = [...this.dataSource.data];
-   
+
   }
 
   detalleCurso(cursoId: number): void {
